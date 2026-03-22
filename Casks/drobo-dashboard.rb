ef0a7b0255ccfa6cca6b18761e2b47fa8fd12630
James Cuzella <james.cuzella@lyraphase.com>
2026-03-22 07:02:16 -0600

    drobo-dashboard: Fix RuboCop brew verified URL warning
    RuboCop warning was:
    
        brew style --cask  lyraphase/right2repair/drobo-dashboard
        Taps/lyraphase/homebrew-right2repair/Casks/drobo-dashboard.rb:6:17: C: [Correctable] Cask/Url: Verified URL parameter value should end with a /.
              verified: "files.drobo.com/webrelease/dashboard/Drobo-Dashboard-"
                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
        1 file inspected, 1 offense detected, 1 offense autocorrectable

--- Casks/drobo-dashboard.rb
+++ Casks/drobo-dashboard.rb
@@ -3,7 +3,7 @@ cask "drobo-dashboard" do
   sha256 "65379a749af92e520d59560c5270e4e29888553f14f29626db90ea89f9e87fab"
 
   url "https://files.drobo.com/webrelease/dashboard/Drobo-Dashboard-#{version.csv.first}.dmg",
-      verified: "files.drobo.com/webrelease/dashboard/Drobo-Dashboard-"
+      verified: "files.drobo.com/webrelease/dashboard/"
   name "Drobo Dashboard"
   desc "Management software and drivers for Data Robotics storage devices"
   homepage "https://web.archive.org/web/20240208020709/https://www.drobo.com/"
